e19f8c0d80b7d52c90544ed723bc7c78a915a29e
Gabor Csizmadia <gaborcsiz@gmail.com>
2020-04-27 00:13:52 +0200

    Add explainers

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import IconButton from '@material-ui/core/IconButton';
 import InputAdornment from '@material-ui/core/InputAdornment';
+import Link from '@material-ui/core/Link';
 import TextField from '@material-ui/core/TextField';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -47,11 +48,16 @@ const useStyles = makeStyles((theme) => ({
     ...theme.mixins.toolbar,
     justifyContent: 'flex-start',
   },
-  divider: {
-    marginBottom: 12
+  drawerBody: {
+    padding: "16px 8px",
+    display: "flex",
+    flexDirection: "column"
   },
   textField: {
-    margin: "12px 16px"
+    marginBottom: 16,
+  },
+  explainers: {
+    marginTop: 8
   }
 }));
 
@@ -116,58 +122,75 @@ function App() {
               <ChevronRightIcon />
             </IconButton>
           </div>
-          <Divider className={classes.divider} />
-          <PercentageField
-            label="Infected on day 0"
-            value={infectedPercentOnDay0}
-            setValue={setInfectedPercentOnDay0}
-            inputProps={{ step: 0.0001, min: 0, max: 100 }}
-          />
-          <Field
-            label="R0 without lockdown"
-            value={r0WithoutLockdown}
-            setValue={setR0WithoutLockdown}
-            inputProps={{ step: 0.1, min: 0 }}
-          />
-          <Field
-            label="R0 with lockdown"
-            value={r0WithLockdown}
-            setValue={setR0WithLockdown}
-            inputProps={{ step: 0.1, min: 0 }}
-          />
-          <Field
-            label="Average recovery time"
-            value={averageRecoveryTime}
-            setValue={setAverageRecoveryTime}
-            inputProps={{ step: 1, min: 0 }}
-            InputProps={{
-              endAdornment: <InputAdornment position="end">days</InputAdornment>,
-            }}
-          />
-          <PercentageField
-            label="Fatality (IFR) under healthcare capacity"
-            value={ifrPercentUnderHealthcareCapacity}
-            setValue={setIfrPercentUnderHealthcareCapacity}
-            inputProps={{ step: 0.1, min: 0, max: 100 }}
-          />
-          <PercentageField
-            label="Fatality (IFR) over healthcare capacity"
-            value={ifrPercentOverHealthcareCapacity}
-            setValue={setIfrPercentOverHealthcareCapacity}
-            inputProps={{ step: 0.1, min: 0, max: 100 }}
-          />
-          <PercentageField
-            label="Healthcare capacity"
-            value={healthcareCapacityPercent}
-            setValue={setHealthcareCapacityPercent}
-            inputProps={{ step: 1, min: 0, max: 100 }}
-          />
-          <Field
-            label="Last day of simulation"
-            value={lastDay}
-            setValue={setLastDay}
-            inputProps={{ step: 1, min: 10 }}
-          />
+          <Divider />
+          <div className={classes.drawerBody}>
+            <PercentageField
+              label="Infected on day 0"
+              value={infectedPercentOnDay0}
+              setValue={setInfectedPercentOnDay0}
+              inputProps={{ step: 0.0001, min: 0, max: 100 }}
+            />
+            <Field
+              label="R0 without lockdown"
+              value={r0WithoutLockdown}
+              setValue={setR0WithoutLockdown}
+              inputProps={{ step: 0.1, min: 0 }}
+            />
+            <Field
+              label="R0 with lockdown"
+              value={r0WithLockdown}
+              setValue={setR0WithLockdown}
+              inputProps={{ step: 0.1, min: 0 }}
+            />
+            <Field
+              label="Average recovery time"
+              value={averageRecoveryTime}
+              setValue={setAverageRecoveryTime}
+              inputProps={{ step: 1, min: 0 }}
+              InputProps={{
+                endAdornment: <InputAdornment position="end">days</InputAdornment>,
+              }}
+            />
+            <PercentageField
+              label="Fatality (IFR) under healthcare capacity"
+              value={ifrPercentUnderHealthcareCapacity}
+              setValue={setIfrPercentUnderHealthcareCapacity}
+              inputProps={{ step: 0.1, min: 0, max: 100 }}
+            />
+            <PercentageField
+              label="Fatality (IFR) over healthcare capacity"
+              value={ifrPercentOverHealthcareCapacity}
+              setValue={setIfrPercentOverHealthcareCapacity}
+              inputProps={{ step: 0.1, min: 0, max: 100 }}
+            />
+            <PercentageField
+              label="Healthcare capacity"
+              value={healthcareCapacityPercent}
+              setValue={setHealthcareCapacityPercent}
+              inputProps={{ step: 1, min: 0, max: 100 }}
+            />
+            <Field
+              label="Last day of simulation"
+              value={lastDay}
+              setValue={setLastDay}
+              inputProps={{ step: 1, min: 10 }}
+            />
+            <div className={classes.explainers}>
+              <Typography variant="caption" paragraph>
+                <b>R0 (basic reproduction number)</b>:
+                number of cases directly generated by one case in a population where all individuals are susceptible to infection
+              </Typography>
+              <Typography variant="caption" paragraph>
+                <b>IFR (infection fatality rate)</b>:
+                proportion of deaths among all the infected individuals (including asymptomatic and undiagnosed infections)
+              </Typography>
+              <Typography variant="caption">
+                The simulation is based on the <SirModelLink>SIR model</SirModelLink>.
+                This is a really simple model, and like all models, it is inaccurate, so the results shouldn't be taken too seriously.
+                For details, feel free to check the <SimulatorCodeLink>code</SimulatorCodeLink>.
+              </Typography>
+            </div>
+          </div>
         </Drawer>
       </div>
     </ThemeProvider>
@@ -202,5 +225,23 @@ function Field(props) {
 const changeHandler = setValue => event => {
   setValue(event.target.value);
 };
+
+const SirModelLink = props => (
+  <Link
+    href="https://en.wikipedia.org/wiki/Compartmental_models_in_epidemiology#The_SIR_model"
+    target="_blank"
+  >
+    {props.children}
+  </Link>
+);
+
+const SimulatorCodeLink = props => (
+  <Link
+    href="https://github.com/gaborcs/lockdown/blob/master/src/simulator.js"
+    target="_blank"
+  >
+    {props.children}
+  </Link>
+);
 
 export default App;
